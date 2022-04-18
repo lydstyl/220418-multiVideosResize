36577dd0c72727ebc9ca2c78432b5d29847a61f9
Gabriel Brun <lydstyl@gmail.com>
2022-04-19 00:56:41 +0200

    Script is working and can resize videos in a folder.

--- resize.sh
+++ resize.sh
@@ -1,14 +1,64 @@
 #!/bin/bash
 
-# 480p (720 x 480)
-# 720p (1280 x 720)
-# 1080p (1900 x 1080)
-# 4k (3840 x 2160)
-# 8k (7680 x 4320)
+pathToVideos=$1
+maxWidth=$2
+outputFolder="output"
 
-# exemple of ffmpeg command for 1 file :
-# ffmpeg -i in.mp4 -vf scale=320:-1 320pxWidthOut.mp4
+if [ -z "$pathToVideos" ]
+then
+      echo "\$pathToVideos is empty"
 
-mkdir ../mp4_w480
+      echo ""
+      echo "Exemple of valid command for max width of 320px --> ./resize.sh ~/my/videos/directory 320"
 
-for f in *.mp4 ; do ffmpeg -i "$f" -vf scale=480:-1 "../mp4_w480/$f" ;  done
+      echo ""
+      echo "480p (720 x 480)"
+      echo "720p (1280 x 720)"
+      echo "1080p (1900 x 1080)"
+      echo "4k (3840 x 2160)"
+      echo "8k (7680 x 4320)"
+
+      echo ""
+      echo "Exemple of ffmpeg command used in this script for 1 file :"
+      echo "ffmpeg -i in.mp4 -vf scale=320:-1 320pxWidthOut.mp4"
+
+else
+      if [ -z "$maxWidth" ]
+      then
+            echo "\$maxWidth is empty"
+            maxWidth="320"
+            echo "\$maxWidth is now $maxWidth"
+      else
+            echo "\$maxWidth is NOT empty"
+            echo "\$maxWidth is $maxWidth"
+      fi
+
+      echo "makedir $pathToVideos/$maxWidth"
+      mkdir "$pathToVideos/$maxWidth"
+
+      echo ""
+      echo "cd $pathToVideos"
+      cd $pathToVideos
+
+      echo ""
+      echo "pwd"
+      pwd
+
+      echo ""
+      echo "Looping thrue files."
+      for f in *.mp4 ; 
+      do 
+            echo ""
+            echo $f ;  
+            echo "./$maxWidth/$f" ; 
+
+            echo ""
+            echo ""
+            echo "ffmpeg -i '$f' -vf scale=$maxWidth:-1 './$maxWidth/$f' "
+            echo ""
+            echo ""
+            ffmpeg -i "$f" -vf scale=$maxWidth:-1 "./$maxWidth/$f" ;
+      done
+
+      echo "Done !"
+fi
